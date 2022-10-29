5224bf0b8b82f92cb456da138e773a02570d0304
kannans <kannansvmail@gmail.com>
2022-10-29 16:44:19 +0530

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import '../scss/styles.scss'
+
+import * as bootstrap from 'bootstrap'
+
+//  specify which plugins you need:
+//import Alert from 'bootstrap/js/dist/alert'
+//  or, specify which plugins you need:
+//import { Tooltip, Toast, Popover } from 'bootstrap'
